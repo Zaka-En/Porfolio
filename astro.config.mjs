00107f9296340b70d2708f30ae0c391220af1a7a
Zaka-En <zakariaennahouti@gmail.com>
2026-03-24 17:25:01 +0100

    chore: update astro config renaming the base to /name-of-the-repo/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   devToolbar: {
     enabled: false,
   },
-  site: 'https://zakadev.netlify.app/', 
+  site: "https:/ezaka.es/",
+  base: "/Porfolio/",
 });
-
